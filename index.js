ad92ea1e271f21406b723e727e651bb4347a7036
manoti <manotimike@gmail.com>
2023-03-13 22:04:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  // let ride = 2300
+  let result
+  if (ride <= 400) {
+    result ='This one is on me!'
+  } else if (ride > 2000 && ride < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (ride > 2500) {
+    result = 'No can do.'
+  }
+
+  return result
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default: 
+    return 'Bye.'
+  }
 }
